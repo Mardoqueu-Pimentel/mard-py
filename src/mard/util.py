f07edb3d3cbe94bbde629a7357f46d19ba69e58b
Mardoqueu Pimentel <mardoqueu.pimentel@gmail.com>
2020-03-15 23:25:49 -0400

    using a global logger instead, dropping the use of logger.exception
    new functionality: try_k_times_or_default
    try getting the result of a function k times or return a default value

--- src/mard/util.py
+++ src/mard/util.py
@@ -4,6 +4,8 @@ from functools import wraps
 from os import getpid
 from typing import Callable, TypeVar, Union, Dict
 
+logger = logging.getLogger(__file__)
+
 T1 = TypeVar('T1')
 T2 = TypeVar('T2')
 T3 = TypeVar('T3')
@@ -15,16 +17,34 @@ Decorator = Callable[[Function], DecoratedFunction]
 
 def try_or_default(default_value: T3 = None) -> Decorator:
 	def decorator(f: Function) -> DecoratedFunction:
-		logger_name = f'{__name__}.{try_or_default.__name__}'
-		logger = logging.getLogger(logger_name)
-
 		@wraps(f)
 		def wrapper(*args, **kwargs):
 			value = default_value
 			try:
 				value = f(*args, **kwargs)
 			except Exception as error:
-				logger.exception(f'error while calling f: {error}')
+				logger.error(f'error while calling {f.__name__}: {error}')
+			return value
+
+		return wrapper
+
+	return decorator
+
+
+def try_k_times_or_default(k: int, default_value: T3 = None) -> Decorator:
+	assert k >= 2, f'use {try_or_default.__name__} instead'
+
+	def decorator(f: Function) -> DecoratedFunction:
+		@wraps(f)
+		def wrapper(*args, **kwargs):
+			value = default_value
+			for i in range(k):
+				try:
+					value = f(*args, **kwargs)
+					break
+				except Exception as error:
+					message = f'try({i + 1}) - error while calling {f.__name__}: {error}'
+					logger.error(message)
 			return value
 
 		return wrapper
